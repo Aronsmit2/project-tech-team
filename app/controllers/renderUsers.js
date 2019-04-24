63c6f0e5bdcb2adab63171b918fdee80ca05dd61
Rick <rickmichiel@icloud.com>
2019-04-24 15:36:07 +0200

    Add matching users based on beer taste function

--- app/controllers/renderUsers.js
+++ app/controllers/renderUsers.js
@@ -1,4 +1,5 @@
 const _array = require("lodash/array");
+const _collection = require("lodash/collection");
 const fetch = require("node-fetch");
 const User = require("../models/User");
 const convertToObject = require("../helpers/convertToObject");
@@ -12,22 +13,53 @@ async function renderUsers(req, res, next) {
         const loggedInUser = await User.findOne({
             "username": req.session.user.username
         });
-        const users = await User.find();
-        const extractIds = users.map(user => user.id);
+        const extractIds = [];
+        await User.find({}, (err, data) => {
+          if (err) console.log(err);
 
-        if (personid) {
-            loggedInUser.likedpersons.push(personid);
-            loggedInUser.save();
-        }
-        
-        for (let i = 0; i < loggedInUser.likedpersons.length; i++) {
-            _array.pull(extractIds, loggedInUser.likedpersons[i]);
-        }
+          for (var i = 0; i < req.session.user.beers.length; i++) {
 
-        convertToObject(extractIds, notLikedUsers);
+            for (var j = 0; j < data.length; j++) {
+
+              // console.log(data[j].beers);
+
+              if (data[j].beers != null) {
+
+                for (var k = 0; k < data[j].beers.length; k++) {
+                  // console.log(data[j].beers[k].beer);
+
+                  if (_collection.includes([data[j].beers[k].beer.bid], req.session.user.beers[i].beer.bid) == true) {
+                    const matchUser = data[j]._id.toString();
+
+                    if (matchUser != loggedInUser._id.toString()) {
+                      extractIds.push(matchUser)
+                    }
+                  }
+                }
+
+              }
+
+            }
+          }
+        });
+
+
+        // const extractIds = users.map(user => user.id);
+
+        // if (personid) {
+        //     loggedInUser.likedpersons.push(personid);
+        //     loggedInUser.save();
+        // }
+        //
+        // for (let i = 0; i < loggedInUser.likedpersons.length; i++) {
+        //     _array.pull(extractIds, loggedInUser.likedpersons[i]);
+        // }
+
+        convertToObject(_array.uniq(extractIds), notLikedUsers);
 
         for (let i = 0; i < 430; i++) {
-            const imageUrl = fetch ("https://source.unsplash.com/collection/181462/480x480");
+            // const imageUrl = fetch ("https://source.unsplash.com/collection/181462/480x480");
+            const imageUrl = ""
             userImages.push(imageUrl);
         }
 
